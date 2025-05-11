f6e191f290afc7672f8b65749a9b1a208d8993ca
Anurag Bhardwaj <86941924+theanuraaag@users.noreply.github.com>
2025-05-12 00:14:47 +0530

    fixed an typo and auth issue

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-    
+        Anurag Bhardwaj
     </div>
   );
 }
--- components/convex-client-provider.jsx
+++ components/convex-client-provider.jsx
@@ -5,5 +5,10 @@ import { ConvexProvider, ConvexReactClient } from "convex/react";
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return <ConvexProvider 
+  client={convex}
+  // useAuth = {}
+  >
+    {children}
+    </ConvexProvider>;
 }
